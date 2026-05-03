c15828b82dcca030defb6d1020ad210d03226049
BloxFruit-Giveaway <kiffymisty@gmail.com>
2026-05-03 16:54:09 +0800

    Update login.js

--- login.js
+++ login.js
@@ -3,56 +3,6 @@ const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium");
 const API_KEY = "CAP-FB4B0BEB9EA6F42CE6CF95EA2CB8FDEE955782A1469C2F5C3F4940C5A7BFC89B";
 
-async function createTask() {
-  const res = await axios.post("https://api.capsolver.com/createTask", {
-    clientKey: API_KEY,
-    task: {
-      type: "FunCaptchaTaskProxyless",
-      websiteURL: "https://www.roblox.com",
-      websitePublicKey: "A2A14B1D-1AF3-C791-9BBC-EE33CC7A0A6F",
-      data: {
-        blob: "This value is different each time, it's in the header as rbx challenge, base64"
-      }
-    }
-  });
-  if (res.data.errorId !== 0) {
-    throw new Error(res.data.errorDescription || "createTask failed");
-  }
-  return res.data.taskId;
-}
-
-async function getResult(taskId) {
-  while (true) {
-    const res = await axios.post("https://api.capsolver.com/getTaskResult", {
-      clientKey: API_KEY,
-      taskId: taskId
-    });
-
-    const data = res.data;
-    if (data.errorId !== 0) {
-      throw new Error(data.errorDescription || "getTaskResult failed");
-    }
-    if (data.status === "ready") {
-      return data.solution;
-    }
-    await new Promise(r => setTimeout(r, 3000));
-  }
-}
-
-
-async function main() {
-  try {
-    console.log("Creating task...");
-    const taskId = await createTask();
-    console.log("Task ID:", taskId);
-    console.log("Waiting for solution...");
-    const solution = await getResult(taskId);
-    console.log("Solved:", solution);
-  } catch (err) {
-    console.error("Error:", err.message);
-  }
-}
-
 const fakeType = (page, input, value) => {
   return new Promise((resolve, reject) => {
     const baseTime = 100;
@@ -95,13 +45,8 @@ async function login(username, password)
     await page.click('button[type="submit"]');
   }
 
-  const hasCaptcha = await page.$(".captcha, iframe[src*='captcha'], div[id*='captcha']");
   try {
     await new Promise(resolve => setTimeout(resolve, 5000));
-    if (hasCaptcha) {
-      console.log("Captcha detected");
-      main()
-    }
     const cookies = await page.cookies();
     let sessionCookie;
     

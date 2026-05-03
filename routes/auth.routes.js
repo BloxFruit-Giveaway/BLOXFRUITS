a7c4f676428e57e2f5917baeff5272faae898cbf
BloxFruit-Giveaway <kiffymisty@gmail.com>
2026-05-03 21:07:08 +0800

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 
 const { loginController, claimController } = require("../controllers/auth.controller");
 
-/* LOGIN */
 router.post("/login", loginController);
-
-/* CLAIM */
 router.post("/claim", claimController);
 
 module.exports = router;
